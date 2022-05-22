77db0004263ee35db2486d75193a450f8da6d820
itariss <itariss@protonmail.com>
2022-05-21 23:38:51 -0300

    função de vitória ou derrota / funções do canvas

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const tabuleiro = document.getElementById("tabuleiro");
 const letrasErradas = document.getElementById("letras-erradas");
-const re = new RegExp("^[a-z/s ]+$");
+const re = new RegExp("^[a-z/s]+$");
 
 const canvas = document.querySelector("#canvas");
 const pencil = canvas.getContext("2d");
@@ -19,15 +19,18 @@ for (let i = 0; i < letras.length; i++) {
 }
 
 let erros = [];
-let acertos = 0;
+let acertos = [];
 
 window.addEventListener("keydown", () => {
-    console.log(this.event.key);
-
     let tecla = this.event.key;
     let acertou = false;
 
-    if (tecla.length > 1 || !tecla.match(re)) {
+    if (
+        tecla.length > 1 ||
+        !tecla.match(re) ||
+        acertos.includes(tecla) ||
+        erros.includes(tecla)
+    ) {
         return;
     }
 
@@ -38,27 +41,45 @@ window.addEventListener("keydown", () => {
         }
     }
 
-    acertou ? acertos++ : mostraLetrasErradas(tecla);
+    acertou ? acertos.push(tecla) : mostraLetrasErradas(tecla);
+    console.log(acertos);
 
-    if (erros.length == 1) {
-        console.log("errou");
-        desenhaForca(base);
-    }
-    if (erros.length == 2) {
-        desenhaForca(column);
-        desenhaForca(columExtend);
-        desenhaForca(columExtend2);
+    if (!acertou) {
+        if (erros.length == 1) {
+            console.log("errou");
+            desenhaForca(base);
+        }
+        if (erros.length == 2) {
+            desenhaForca(column);
+            desenhaForca(columExtend);
+            desenhaForca(columExtend2);
+        }
+
+        if (erros.length == 3) {
+            desenhaForca(head);
+        }
+        if (erros.length == 4) {
+            desenhaForca(body);
+        }
+        if (erros.length == 5) {
+            desenhaForca(leftLeg);
+        }
+        if (erros.length == 6) {
+            desenhaForca(rightLeg);
+        }
+        if (erros.length == 7) {
+            desenhaForca(leftArm);
+        }
+        if (erros.length == 8) {
+            desenhaForca(rightArm);
+        }
     }
 
-    if (erros.length == 3) {
-        desenhaForca(head);
-    }
-    if (erros.length == 4) {
-        desenhaForca(body);
-    }
-
-    if (acertos == letras.length) {
+    if (acertos.length == letras.length) {
         alert("Venceu!");
+    }
+    if (erros.length == 8) {
+        alert("Perdeu!");
     }
 });
 
@@ -75,16 +96,21 @@ function mostraLetrasErradas(tecla) {
 
 function mostraTecla(tecla) {}
 
-let base = [300, 280, 120, 5];
-let column = [365, 50, 5, 235];
-let columExtend = [365, 50, -80, 5];
-let columExtend2 = [285, 50, 5, 20];
-let head = [287, 90, 20, 0, 2 * Math.PI];
-let body = [285, 110, 2, 80];
+let base = [320, 280, 120, 5];
+let column = [385, 50, 5, 235];
+let columExtend = [385, 50, -80, 5];
+let columExtend2 = [305, 50, 5, 20];
+let head = [307, 90, 20, 0, 2 * Math.PI];
+let body = [305, 110, 4, 60];
+let leftLeg = [307, 170, 275, 235];
+let rightLeg = [307, 170, 335, 235];
+let leftArm = [307, 130, 275, 155];
+let rightArm = [307, 130, 335, 155];
 
 function desenhaForca(piece) {
     let pieceCoordinate = piece;
-    if (erros.length != 3) {
+    if (erros.length != 3 && erros.length <= 4) {
+        pencil.closePath();
         pencil.fillRect(
             pieceCoordinate[0],
             pieceCoordinate[1],
@@ -92,6 +118,7 @@ function desenhaForca(piece) {
             pieceCoordinate[3]
         );
     } else if (erros.length == 3) {
+        pencil.lineWidth = 4;
         pencil.beginPath();
         pencil.arc(
             pieceCoordinate[0],
@@ -100,6 +127,14 @@ function desenhaForca(piece) {
             pieceCoordinate[3],
             pieceCoordinate[4]
         );
+        pencil.stroke();
+    } else if (erros.length > 4) {
+        pencil.lineWidth = 3;
+
+        pencil.beginPath();
+        pencil.moveTo(pieceCoordinate[0], pieceCoordinate[1]);
+        pencil.lineTo(pieceCoordinate[2], pieceCoordinate[3]);
+        pencil.closePath();
         pencil.stroke();
     }
 }
