a81619b4cf91147a8b81ade3deb171489f14cd25
itariss <itariss@protonmail.com>
2022-05-31 17:45:02 -0300

    lógica para adicionar nova palavra + nova dica como objeto

--- assets/js/new.js
+++ assets/js/new.js
@@ -1,6 +1,11 @@
 const send = document.querySelector("#add");
+const addTip = document.querySelector("#add-tip");
+const addWord = document.querySelector("#add-word");
 const addField = document.getElementById("campo-adiciona");
 const newGame = document.querySelector("#new-game");
+
+let setTip = false;
+
 let novasPalavras = [];
 
 newGame.onclick = event => {
@@ -9,15 +14,38 @@ newGame.onclick = event => {
         JSON.stringify(novasPalavras)
     );
 };
+let novaPalavra;
 
 send.onclick = event => {
     if (!addField.validity.patternMismatch && addField.value != "") {
-        let novaPalavra = {};
-        event.preventDefault();
-        novaPalavra.nome = addField.value;
-        novasPalavras.push(novaPalavra);
-        console.log(novasPalavras);
-        addField.value = "";
+        let qntsPalavras = 1;
+        if (!setTip) {
+            novaPalavra = {};
+            addWord.style.display = "none";
+            addWord.classList.remove("animate__lightSpeedInRight");
+            event.preventDefault();
+            novaPalavra.nome = addField.value;
+            console.log(novasPalavras);
+            console.log(addTip);
+            addTip.classList.add("animate__lightSpeedInRight");
+            addTip.style.display = "flex";
+            addField.value = "";
+            setTip = true;
+            return novaPalavra;
+        } else if (setTip) {
+            addTip.classList.remove("animate__lightSpeedInRight");
+            addTip.style.display = "none";
+            event.preventDefault();
+            novaPalavra.dica = addField.value;
+            novasPalavras.push(novaPalavra);
+            console.log(novasPalavras);
+            console.log(addTip);
+            addWord.classList.add("animate__lightSpeedInRight");
+            addWord.style.display = "flex";
+            addField.value = "";
+            setTip = false;
+            qntsPalavras += 1;
+        }
     }
 };
 
